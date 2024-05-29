67ee915a040d5dde219045cf665ac284d7490df9
tobiasgmueller <tgmueller@ucdavis.edu>
2024-05-29 12:38:40 -0400

    created phyloseq for ITS script - but ITS file only has 20 samples. something went weird in that pipeline

--- code/phyloseq_ITS.R
+++ code/phyloseq_ITS.R
@@ -6,7 +6,10 @@
 if(!requireNamespace("BiocManager")){
   install.packages("BiocManager")
 }
-BiocManager::install("phyloseq")
+
+if(!requireNamespace("phyloseq")){
+  install.packages("phyloseq")
+}
 
 library(phyloseq); packageVersion("phyloseq")
 library(tidyverse)
@@ -14,8 +17,8 @@ library(tidyverse)
 
 # read in dada2 16s pipeline output 
 
-taxa <- readRDS("input/processed_sequences/16s/taxa.rds")
-seqtab.nochim <- readRDS("input/processed_sequences/16s/seqtab_nochim.rds")
+taxa <- readRDS("input/processed_sequences/ITS/ITS_taxa.rds")
+seqtab.nochim <- readRDS("input/processed_sequences/ITS/seqtab_nochim.rds")
 
 
 
@@ -53,49 +56,16 @@ ps
 # write these raw tables
 
 write.table(tax_table(ps),
-            "input/processed_sequences/16s/tables/full_tax_table_16s.txt",
+            "input/processed_sequences/ITS/full_tax_table_ITS.txt",
             sep="\t", quote = FALSE, col.names=NA)
 
 write.table(t(otu_table(ps)),
-            "input/processed_sequences/16s/tables/full_seq_table_16s.txt",
+            "input/processed_sequences/ITS/full_seq_table_ITS.txt",
             sep="\t", quote = FALSE, col.names=NA)
 
 
 
 
-# check for mitochondria, chloroplast, Eukaryotas
-
-"Chloroplast" %in% tax_table(ps)
-"Mitochondria" %in% tax_table(ps)
-"Eukaryota" %in% tax_table(ps)
-
-#this still has alot of mitochondrial and chloroplast reads so we'll remove those
-# remove mitochondria and chloroplast
-
-
-
-# generate a file with mitochondria ASVs
-MT1 <- subset_taxa(ps, Family == "Mitochondria")
-MT1 <-  as(tax_table(MT1), "matrix")
-MT1 <- MT1[, (ncol(MT1))]
-MT1df <- as.factor(MT1)
-goodTaxa <- setdiff(taxa_names(ps), MT1df)
-ps_no_mito <- prune_taxa(goodTaxa, ps)
-ps_no_mito
-
-
-CH1 <- subset_taxa(ps_no_mito, Order == "Chloroplast")
-CH1 <-  as(tax_table(CH1), "matrix")
-CH1 <- CH1[, (ncol(CH1))]
-CH1df <- as.factor(CH1)
-goodTaxa <- setdiff(taxa_names(ps_no_mito), CH1df)
-ps_no_chloro <- prune_taxa(goodTaxa, ps_no_mito)
-ps_no_chloro
-
-
-
-# then set that to ps for easy coding
-ps<-ps_no_chloro
 
 # plot richness
 plot_richness(ps, x="treatment", measures=c("Shannon", "Simpson"), color="day")
@@ -115,10 +85,10 @@ plot_ordination(ps.prop, ord.nmds.bray, color="day", title="Bray NMDS")
 
 # make bar chart
 
-top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
-ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
-ps.top20 <- prune_taxa(top20, ps.top20)
-plot_bar(ps.top20, x="treatment", fill="Family") + facet_wrap(~day, scales="free_x")
+top100 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:100]
+ps.top100 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
+ps.top100 <- prune_taxa(top100, ps.top100)
+plot_bar(ps.top20, x="treatment", fill="Class") + facet_wrap(~day, scales="free_x")
 
 
 plot_bar(ps.top20, x="treatment", fill="Genus") 
